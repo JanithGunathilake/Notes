6c3a19e6f52ca158f17c54b48792337b6450d58c
JanithGunathilake <janithkavinda49@gmail.com>
2023-07-08 17:02:10 +0530

    Adapter_Done

--- app/src/main/java/com/example/notes/Database/NoteRepository.kt
+++ app/src/main/java/com/example/notes/Database/NoteRepository.kt
@@ -1,4 +1,22 @@
 package com.example.notes.Database
 
-class NoteRepository {
+import androidx.lifecycle.LiveData
+import com.example.notes.Models.Note
+
+class NoteRepository(private val noteDao: NoteDao) {
+
+    val allNote : LiveData<List<Note>> = noteDao.getAllNotes()
+
+    suspend fun insert(note: Note){
+        noteDao.insert(note)
+    }
+
+    suspend fun delete(note: Note){
+        noteDao.delete(note)
+    }
+
+    suspend fun update(note: Note){
+        noteDao.update(note.id,note.title,note.note)
+    }
+
 }
